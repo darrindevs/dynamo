7f1ddaae5213a34f2b7a35e6446cf29840f69179
Zaino <hello@darrin.dev>
2022-03-05 15:41:58 -0800

    add clicky plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,12 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-          
       },
+      {
+        resolve: `gatsby-plugin-clicky`,
+        options: {
+          siteId: '101297670'
+        },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,6 @@ import { Row } from "react-bootstrap"
 const IndexPage = () => (
   <Layout>
     <Seo title="⚡️ Dynamo" />
-    <script async src="//static.getclicky.com/101297670.js"></script>
-    <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101297670ns.gif" /></p></noscript>
   </Layout>
 )
 
